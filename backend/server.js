e09df482d2031aec9858a2ef4345e086ffe54b25
Ivan Escribano <ivanescribano1998@gmail.com>
2022-09-19 14:34:39 +0200

    Added structure folder , dependecies, scripts and did some tests

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+//TEST TO DELETE: NODEMON IS WORKING "npm run server"
+console.log("Hello world!!!");
+
+require("dotenv").config();
+//TEST TO DELETE : ENV IS WORKING
+console.log(process.env.PORT);
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen bg-fuchsia-800 flex justify-center items-center">
+      <h1 className="text-5xl text-white">BARBER SHOP TEST WITH TAILWIND</h1>
+    </div>
+  );
+}
+
+export default App;
