70ac32da26e981f41746f8456c9786d960227296
Ivan Escribano <ivanescribano1998@gmail.com>
2022-09-20 11:36:03 +0200

    ci(init-config): added connection and some middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
-//TEST TO DELETE: NODEMON IS WORKING "npm run server"
-console.log("Hello world!!!");
-
+//!DEPENDENCIES : EXPRESS
+const express = require("express");
+const app = express();
+//!ADD ENVIRONMENT VARIABLES
 require("dotenv").config();
-//TEST TO DELETE : ENV IS WORKING
+
+//!CONNECT TO SERVER MONGO
+const connect = require("./config/dbConfig");
+connect();
+//!MIDDLEWARES: GET PARAMETERS FROM THE BODY AND JSON PARSER THEM
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 console.log(process.env.PORT);
+//!PORT TO LISTEN
+app.listen(process.env.PORT, () => {
+  console.log(`App express is running in port:`);
+});
