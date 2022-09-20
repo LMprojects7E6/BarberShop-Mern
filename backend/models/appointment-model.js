706acbd779ec957c5c8ce22952de730d1fea41fc
Averoli <afanasenkavolha@gmail.com>
2022-09-20 09:43:58 +0200

    Create Models

---
+++ backend/models/appointment-model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const AppointmentShema = new mongoose.Schema({
+  precio: { type: Number, required: true, trim: true },
+  startData: { type: Data, required: true },
+  endData: { type: Data, required: true },
+  employee: {
+    type: mongoose.SchemaTypes.ObjectId,
+    required: true,
+    ref: "user",
+  },
+  customer: {
+    type: mongoose.SchemaTypes.ObjectId,
+    required: true,
+    ref: "user",
+  },
+});
+
+const AppointmentModel = new mongoose.model("appointment", AppointmentShema);
+
+module.export = AppointmentModel;
