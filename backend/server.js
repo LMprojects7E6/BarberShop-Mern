dd450a93e2d7bfb865889aa3b5e9282f109d9edc
Ivan Escribano <ivanescribano1998@gmail.com>
2022-09-20 11:43:27 +0200

    ci(init-config): added some dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
-//!DEPENDENCIES : EXPRESS
+//!DEPENDENCIES
 const express = require("express");
 const app = express();
+const helmet = require("helmet");
+const cors = require("cors");
 //!ADD ENVIRONMENT VARIABLES
 require("dotenv").config();
 
 //!CONNECT TO SERVER MONGO
 const connect = require("./config/dbConfig");
 connect();
-//!MIDDLEWARES: GET PARAMETERS FROM THE BODY AND JSON PARSER THEM
+
+//!MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-console.log(process.env.PORT);
+app.use(helmet());
+app.use(cors());
+
 //!PORT TO LISTEN
 app.listen(process.env.PORT, () => {
-  console.log(`App express is running in port:`);
+  console.log(`App express is running in port: ${process.env.PORT}`);
 });
