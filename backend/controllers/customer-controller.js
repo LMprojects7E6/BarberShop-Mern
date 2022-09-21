582c7e427b2a51188a040a101ee061e2cedc38b9
reduansj <reduansenoun@gmail.com>
2022-09-21 10:53:16 +0200

    fetaure(customer) chnage customers functions to users

--- backend/controllers/customer-controller.js
+++ backend/controllers/customer-controller.js
@@ -1,26 +1,55 @@
 //CONNECTION TO DATABASE MODELS
 const dbModel = require("../models");
 
-const getCostumers = async (req, res, next) => {
-    try {
-        const employees = await dbModel.User.find({role:"customer"}).lean().exec();
+const getUsersByRole = async (req, res) => {
+  const { role } = req.params;
+  try {
+    const employees = await dbModel.User.find({ role: role }).lean().exec();
 
-        res.status(200).send({ data : employees })
-    } catch (error) {
-        next(error);
-    }
-}
+    res.status(200).send({ data: employees });
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+};
 
-const getCostumersById = (req, res, next) => {
+const getUsersByEmail = async (req, res) => {
+  const { email } = req.params;
 
-}
+  try {
+    const customer = await dbModel.User.find({ email: email });
 
-const deleteCostumer = (req, res, next) => {
-    
-}
+    res.status(200).send(customer);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+};
+
+const getUsersById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const customer = await dbModel.User.find({ id: id });
+
+    res.status(200).send(customer);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const customer = await dbModel.User.remove({ id: id });
+    res.status(200).send(customer);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+};
 
 module.exports = {
-    getCostumers : getCostumers,
-    getCostumersById : getCostumersById,
-    deleteCostumer : deleteCostumer
-}
+  getUsersByRole: getUsersByRole,
+  getUsersById: getUsersById,
+  getUsersByEmail: getUsersByEmail,
+  deleteUser: deleteUser,
+};
