90d7bb5bf5bea37862ba7f9ef53348cd58281980
reduansj <reduansenoun@gmail.com>
2022-09-22 14:32:34 +0200

    fetaure(user-controllers): refactor getUsers & updateUser functions

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -1,25 +1,20 @@
 //CONNECTION TO DATABASE MODELS
 const dbModel = require("../models");
 
-const getAllUsers = async (req, res) => {
-  try {
-    const user = await dbModel.User.find({}).lean().exec();
-
-    res.status(200).send(user);
-  } catch (error) {
-    res.status(404).send({ message: error.message });
-  }
-};
-
 const getUsers = async (req, res) => {
-  const values = req.query;
-  console.log(values);
+  const key = Object.keys(req.query);
+  const value = Object.values(req.query)[0];
+
+  if (!queryToKey[key]) {
+    res.status(400).send({ message: "invalid endpoint" });
+  }
 
   try {
-    values.role
-      ? (user = await dbModel.User.find({ role: values.role }).lean().exec())
-      : (user = await dbModel.User.find({ _id: values.id }).lean().exec());
-
+    const user = await dbModel.User.find({
+      [queryToKey[key]]: value,
+    })
+      .lean()
+      .exec();
     res.status(200).send(user);
   } catch (error) {
     res.status(404).send({ message: error.message });
@@ -55,38 +50,28 @@ const createUser = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-  const { id } = req.params;
-  const { first_name, last_name, email, password } = req.body;
+  const user = await dbModel.User.findById(req.params.id);
 
-  try {
-    const updatedUser = dbModel.User.findOneAndUpdate(
-      {
-        _id: id,
-      },
-      {
-        $set: {
-          first_name: first_name,
-          last_name: last_name,
-          email: email,
-          password: password,
-        },
-      },
-      {
-        new: true,
-      }
-    );
+  if (!user) return res.status(404).send("No user with that id");
 
-    res.status(200).send(updatedUser);
-  } catch (error) {
-    res.status(404).send({ message: error.message });
-  }
+  const updatedUser = await dbModel.User.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
+
+  res.status(200).send(updatedUser);
+};
+
+const queryToKey = {
+  role: "role",
+  id: "_id",
 };
 
 module.exports = {
-  getAllUsers: getAllUsers,
   getUsers: getUsers,
-  // getUsersById: getUsersById,
-  // getUsersByRole: getUsersByRole,
   deleteUser: deleteUser,
   createUser: createUser,
   updateUser: updateUser,
