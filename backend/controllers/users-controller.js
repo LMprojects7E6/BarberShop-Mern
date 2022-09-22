3429ee5ce06f3ce4bb8c55120d7776ab6c8d42d5
reduansj <reduansenoun@gmail.com>
2022-09-22 15:20:24 +0200

    userByEmail

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -52,11 +52,14 @@ const createUser = async (req, res) => {
 
 const updateUser = async (req, res) => {
   const user = await dbModel.User.findById(req.params.id);
-  const userEmail = await dbModel.User.findOne({ email: req.body.email });
+  const userByEmail = await dbModel.User.findOne({ email: req.body.email });
 
   if (!user) return res.status(404).send("No user with that id");
 
-  if (userEmail !== null && userEmail._id.toString() !== user._id.toString())
+  if (
+    userByEmail !== null &&
+    userByEmail._id.toString() !== user._id.toString()
+  )
     return res.status(404).send("This email is already taken");
 
   const updatedUser = await dbModel.User.findByIdAndUpdate(
