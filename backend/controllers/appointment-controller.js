5d0c57ddec16bbf84d3024a25b9a59dc8ab885ce
MiguelD-PC <mikdominguez@gmail.com>
2022-09-23 10:13:50 +0200

    merge commit

--- backend/controllers/appointment-controller.js
+++ backend/controllers/appointment-controller.js
@@ -1,5 +1,6 @@
 //CONNECTION TO DATABASE MODELS
 const dbModel = require("../models");
+const { updateUser } = require("./users-controller");
 
 const getAppointments = async (req, res) => {
   const { id } = req.params;
@@ -44,7 +45,18 @@ const createAppointment = (req, res) => {
       customer,
       date,
     });
+    updateUser(customer, newAppointment);
     res.status(200).send(newAppointment);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+};
+
+const updateUser = async (customer, newAppointment) => {
+  const filter = { _id: customer };
+  const update = { appointments: appointments.push(newAppointment) };
+  try {
+    const updateAppointment = await dbModel.User.updateOne(filter, update);
   } catch (error) {
     res.status(404).send({ message: error.message });
   }
