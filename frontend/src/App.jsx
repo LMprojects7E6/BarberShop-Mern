7d3d7049549b1934b0432728ef1349eb427f516d
Ivan Escribano <ivanescribano1998@gmail.com>
2022-09-25 04:28:46 +0200

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
 import { Toaster } from "react-hot-toast";
 import Router from "./router/Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+const queryClient = new QueryClient();
 function App() {
-  const queryClient = new QueryClient({});
   return (
     <QueryClientProvider client={queryClient}>
       <Router></Router>
