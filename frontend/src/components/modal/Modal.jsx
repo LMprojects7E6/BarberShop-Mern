96f6f34403f3f3495d25f0cbd3239c173000745b
reduansj <reduansenoun@gmail.com>
2022-09-26 11:16:40 +0200

    feat(modal): remove X button and change close to cancel

--- frontend/src/components/modal/Modal.jsx
+++ frontend/src/components/modal/Modal.jsx
@@ -28,14 +28,6 @@ export default function Modal({ modalTitle, buttonType, children }) {
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-3xl font-semibold">{modalTitle}</h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
                 </div>
                 {/*body*/}
 
@@ -51,7 +43,7 @@ export default function Modal({ modalTitle, buttonType, children }) {
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
-                    Close
+                    CANCEL
                   </button>
                 </div>
               </div>
