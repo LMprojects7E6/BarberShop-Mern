7b734bf7750e3d56208c05b4d7f10b33bf1257d5
reduansj <reduansenoun@gmail.com>
2022-09-27 09:36:42 +0200

    feat(appointment): change styles modal

--- frontend/src/pages/customer/deleteAppointment/DeleteAppointment.jsx
+++ frontend/src/pages/customer/deleteAppointment/DeleteAppointment.jsx
@@ -3,6 +3,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { deleteAppointment } from "../../../Api/appointment";
 import DeleteButton from "../../../components/buttons/DeleteButton";
+import Button from "../../../components/buttons/Button";
 const DeleteAppointment = ({ appointmentID }) => {
   console.log(appointmentID);
   const queryClient = useQueryClient();
@@ -21,11 +22,13 @@ const DeleteAppointment = ({ appointmentID }) => {
     deleteAppointmentMutation.mutate(appointmentID);
   };
   return (
-    <div>
+    <div className="flex flex-col text-center">
       <h2 className="text-2xl mb-3">
         Are you sure you want to delete this appointment?
       </h2>
-      <DeleteButton isCustomer={true} onClick={handleSubmit}></DeleteButton>
+      <div className="text-center mt-5">
+        <Button onClick={handleSubmit} text={"DELETE"} />
+      </div>
     </div>
   );
 };
